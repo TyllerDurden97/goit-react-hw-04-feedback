7c1a3d20c345430a7f28d00da2929102babe56d4
TyllerDurden97 <albatros97dn@gmail.com>
2023-05-21 21:48:38 +0300

    refact.in hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-lone-blocks */
-import { Component } from "react";
+import { React, useState } from "react";
 import { Statistics } from 'components/Statistics/Statistics';
 import { FeedbackOptions } from 'components/FeedbackOptions/FeedbackOptions';
 import { Section } from "components/Section/Section";
@@ -7,73 +7,80 @@ import { Notification } from 'components/NotificationMessage/NotificationMessage
 // import { css } from 'components/Section/Section.module.css';
 
 
-export class App extends Component {
-   state = {
-      good: 0,
-      neutral: 0,
-      bad: 0
-   };
-   
-   handleFeedbButton = (e) => {
+export const App = () => {
+   const [good, setGood] = useState(0);
+   const [neutral, setNeutral] = useState(0);
+   const [bad, setBad] = useState(0);
+   // state = {
+   //    good: 0,
+   //    neutral: 0,
+   //    bad: 0
+   // };
+
+   const handleFeedbButton = (e) => {
       const nameOfState = e.currentTarget.name;
-      this.setState((prevState) => ({ [nameOfState]: prevState[nameOfState] + 1 }
-      ));
-      // console.log(this.state.good);
-      // console.log(e.currentTarget.name);
+      // setGood((prevState) => (prevState + 1));
+      // setNeutral((prevState) => ( prevState + 1 ));
+      // setBad((prevState) => ( prevState + 1 )); 
+      // console.log(good);
+
+      switch (nameOfState) {
+         case 'good':
+            setGood((prevState) => (prevState + 1))
+            break;
+         case 'neutral':
+            setNeutral((prevState) => (prevState + 1))
+            break;
+         case 'bad':
+            setBad((prevState) => (prevState + 1))
+            break;
+         default:
+            return;
+      };
+
    };
 
-   countTotalFeedback = ({good, neutral, bad}) => 
+   const countTotalFeedback = () => 
       (good + neutral + bad);
+   console.log(countTotalFeedback() );
    
-   countPositiveFeedbackPercentage = ({ good }) => (
-      this.countTotalFeedback(this.state) ?
-         Math.round(good * 100 / this.countTotalFeedback(this.state)) :
+  const countPositiveFeedbackPercentage = () => (
+      countTotalFeedback() ?
+         Math.round(good * 100 / countTotalFeedback()) :
          0
+   );
 
+   return (
+      <>
+         <Section title={'Please leave feedback'}>
+            <FeedbackOptions options={Object.keys({ good, neutral, bad })} onLeaveFeedback={handleFeedbButton} />
+         </Section >
+         {countTotalFeedback() ?
+            (<Section title={'Statistic'}>
+               <Statistics good={good} neutral={neutral} bad={bad}
+                  total={countTotalFeedback()}
+                  positivePercentage={countPositiveFeedbackPercentage()} />
+            </Section>) : (<Notification message="There is no feedback" />)}
+      </>
    );
    
    
-   render() {
-      const { good, neutral, bad } = this.state;
 
-      return (
-         <>
-         <Section title={'Please leave feedback'}>
-            <FeedbackOptions options={Object.keys(this.state)} onLeaveFeedback={this.handleFeedbButton}/>
-            </Section >
-            {this.countTotalFeedback(this.state) ?
-               (<Section title={'Statistic'}>
-                  <Statistics good={good} neutral={neutral} bad={bad}
-                     total={this.countTotalFeedback(this.state)}
-                     positivePercentage={this.countPositiveFeedbackPercentage(this.state)} />
-               </Section>) : (<Notification message="There is no feedback" />)}
-         </>         
-              )
+      // return (
+      //    <>
+      //    <Section title={'Please leave feedback'}>
+      //       <FeedbackOptions options={Object.keys(this.state)} onLeaveFeedback={this.handleFeedbButton}/>
+      //       </Section >
+      //       {this.countTotalFeedback(this.state) ?
+      //          (<Section title={'Statistic'}>
+      //             <Statistics good={good} neutral={neutral} bad={bad}
+      //                total={this.countTotalFeedback(this.state)}
+      //                positivePercentage={this.countPositiveFeedbackPercentage(this.state)} />
+      //          </Section>) : (<Notification message="There is no feedback" />)}
+      //    </>         
+      //         )
    };
 
 
 
-     /* <div>
-            <h2>Please leave feedback</h2>
-            <button name='good' type='button' onClick={this.handleFeedbButton}>Good</button>
-            <button name='neutral' type='button' onClick={this.handleFeedbButton}>Neutral</button>
-            <button name='bad' type='button' onClick={this.handleFeedbButton}>Bad</button>
-         </div>
-         <div>
-               <h2>Statistic</h2> */}
-            // eslint-disable-next-line no-lone-blocks
-            {/* <Statistics good={this.state.good} neutral={this.state.neutral} bad={this.state.bad} total={this.countTotalFeedback(this.state)} positivePercentage={this.countPositiveFeedbackPercentage(this.state)}> */}
-               
-      
-               {/* <p>Good:<span>{this.state.good}</span></p>
-               <p>Neutral:<span>{this.state.neutral}</span></p>
-               <p>Bad:<span>{this.state.bad}</span></p>
-               <p>Total:<span>{this.countTotalFeedback(this.state)}</span></p>
-               <p>Positive feedback:<span>{this.countPositiveFeedbackPercentage(this.state)}%</span></p> */}
-
-               {/* <span>Neutral:{ this.state.neutral}</span>
-               <span>Bad:{this.state.bad}</span>
-               <span>Total:{this.countTotalFeedback(this.state)}</span>
-               <span>Positive feedback:{this.countPositiveFeedbackPercentage(this.state)}%</span> */}
-
-            {/* </div> */}
+   
--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -5,7 +5,7 @@ import css from 'components/FeedbackOptions/FeedbackOptions.module.css';
 export const FeedbackOptions = ({ options, onLeaveFeedback }) => (
       options.map((option) => {
          return (  
-            <div className={css.feedBbuttonGroup}>
+            <div className={css.feedBbuttonGroup} key={option}>
                <button name={option} type='button' className={css.feedBbutton} onClick={onLeaveFeedback}>{option}</button>
             </div>
             )   
